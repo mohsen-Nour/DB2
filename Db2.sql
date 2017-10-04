688f23284fa04eb493b99f407671977ac5d83942
mohsen-Nour <mohsen.noor@gmail.com>
2017-10-04 13:17:28 +0000

    Update Db2.sql

--- Db2.sql
+++ Db2.sql
@@ -1,2 +1,4 @@
 UPDATE ACCOUNT SET UPLOADINGFILEID = 2 WHERE ACCOUNTID IN 
 (select ACCOUNTID from ACCOUNT where UPLOADINGFILEID = 85  order by ACCOUNTID fetch first 30000 rows only)
+
+this delete partison ways in db2  
